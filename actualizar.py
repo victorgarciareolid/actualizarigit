36f13bd975b38c4ce06537a30d51f39a4c62d3e3
viktoriotw <victorgarciavilu@gmail.com>
2014-10-19 19:19:20 +0200

    Actualizacion

--- actualizar.py
+++ actualizar.py
@@ -1,3 +1,16 @@
 import subprocess
-subprocess.call(['git', 'pull'])
+import os
+import http.server
+import socketserver
 
+class MyHandler(http.server.BaseHTTPRequestHandler):
+	def do_GET(self):
+		subprocess.call(['git', 'pull'])
+		self.send_response(100)
+
+def httpd (handler_class = MyHandler, server_adress =('localhost', 8080)):
+	srvr = http.server.HTTPServer(server_adress, handler_class)
+	srvr.serve_forever()
+
+if __name__ == "__main__":
+	httpd()
